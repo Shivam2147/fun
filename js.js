d835c99094cd0ee6ac6c99c22e549639fa8108f6
shivam <sharmashivam52465@gmail.com>
2024-01-17 19:04:23 +0530

    ok commit

--- js.js
+++ js.js
@@ -3,7 +3,7 @@ let cities = [
     "New York", "London", "Tokyo", "Paris", "Sydney", "Berlin", "Los Angeles", "Mumbai", "Toronto", "Rio de Janeiro",
     "Beijing", "Dubai", "Moscow", "Rome", "Cairo", "Seoul", "Cape Town", "San Francisco", "Bangkok", "Mexico City",
     "Amsterdam", "Madrid", "Istanbul", "Stockholm", "Singapore", "Hong Kong", "Vienna", "Athens", "Prague", "Toronto",
-    "Barcelona","Oslo", "Lisbon", "Edinburgh", "Helsinki", "Brussels", "Copenhagen", "Auckland", "Wellington", "Osaka", "Kyoto"
+    "Barcelona", "Oslo", "Lisbon", "Edinburgh", "Helsinki", "Brussels", "Copenhagen", "Auckland", "Wellington", "Osaka", "Kyoto"
 ];
 cities.forEach((Element) => {
     let div = document.createElement('div');
@@ -11,12 +11,12 @@ cities.forEach((Element) => {
     let yPos = Math.random() * 100;
     div.classList.add('city')
     div.style.top = `${xPos}%`
-    div.style.left = `${yPos}%`    
+    div.style.left = `${yPos}%`
     div.innerText = Element;
     main.appendChild(div)
-    main.addEventListener('mousemove',()=>{    
+    main.addEventListener('mousemove', () => {
         xPos = Math.random() * 100;
-        yPos = Math.random() * 100;    
+        yPos = Math.random() * 100;
         div.style.transform = `translate(${-xPos}%,${-yPos}%)`
     })
 
